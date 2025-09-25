44a11d3a19e236f3245c6a614399f1e7064aed45
adnan ahmad <adnan@MacBook-Pro-2.local>
2025-09-25 17:35:45 +0100

    update multi lang

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,10 @@
-const inputs = ["C is fun", "python is cool","Javascript is amazing"];
+const inputs = ["C is fun", "Python is cool","Javascript is amazing"];
 
 for (const input of inputs){
     console.log(input);
 }
+
+// let counter = 0;
+// while (counter < inputs.length){
+//     console.log(counter);
+// }
