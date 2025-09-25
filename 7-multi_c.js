e537b5bb1c37042eef8f1b1f3b4065b20f4fd809
adnan ahmad <adnan@MacBook-Pro-2.local>
2025-09-25 21:12:33 +0100

    mulit c

---
+++ 7-multi_c.js
@@ -0,0 +1,12 @@
+const input = process.argv.slice(2);
+
+let num = parseInt(input[0]);
+
+if (isNaN(num)){
+    console.log("Missing number of occurrences");
+}
+else {
+    for (let i = 0; i < num; i++){
+        console.log("C is fun");
+    }
+}
