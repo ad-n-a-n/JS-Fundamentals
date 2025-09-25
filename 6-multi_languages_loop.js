f3d5531757912f6e3df426b99f8ffa802233ff1a
adnan ahmad <adnan@MacBook-Pro-2.local>
2025-09-25 17:41:23 +0100

    using while loop

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,12 +1,12 @@
 const inputs = ["C is fun", "Python is cool","JavaScript is amazing"];
 
-for (const input of inputs){
-    console.log(input);
-}
-
-// let counter = 0;
-// while (counter < inputs.length){
-//     // console.log(counter);
-//     console.log(inputs[counter]);
-//     counter++;
+// for (const input of inputs){
+//     console.log(input);
 // }
+
+let counter = 0;
+while (counter < inputs.length){
+    // console.log(counter);
+    console.log(inputs[counter]);
+    counter++;
+}
