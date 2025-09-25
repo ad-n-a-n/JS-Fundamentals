99601e568c0eb416dc4db0dab8cd0a964d17d920
adnan ahmad <adnan@MacBook-Pro-2.local>
2025-09-25 02:42:36 +0100

    modify value argument

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,12 +1,24 @@
+// const input = process.argv.slice(2);
+
+// if (input.length === 1) {
+//   console.log("Argument found");
+
+// }
+// else if (input.length > 1){
+//     console.log("Arguments found");
+// }
+//  else {
+//   console.log("No argument");
+// }
+
 const input = process.argv.slice(2);
 
-if (input.length === 1) {
-  console.log("Argument found");
-
+if (!input.length) {
+    console.log("No argument");
 }
-else if (input.length > 1){
-    console.log("Arguments found");
+else if (input.length === 1){
+    console.log("Argument found");
 }
- else {
-  console.log("No argument");
+else {
+    console.log("Arguments found")
 }
