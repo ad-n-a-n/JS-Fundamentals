50c7ff2d977dd1dff4a242e888bd38b08d273998
adnan ahmad <adnan@MacBook-Pro-2.local>
2025-10-02 03:27:20 +0100

    factorial

---
+++ 10-factorial.js
@@ -0,0 +1,16 @@
+let fact = parseInt(process.argv[2]);
+
+function factorial(n){
+
+if (Number.isNaN(n)){
+    return 1;
+}
+if (n <= 1) {
+    return 1;
+    
+}
+else {
+    return n * factorial(n - 1);
+}
+}
+console.log(factorial(fact));
